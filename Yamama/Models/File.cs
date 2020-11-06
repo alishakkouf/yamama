9c60b2e80360742ab52cc78069eaf65723e40baa
ali <a_shakkouf@prokoders.com>
2020-11-06 20:06:28 +0200

    batoul merged

--- Yamama/Models/File.cs
+++ Yamama/Models/File.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Yamama
+namespace Yamama.Models
 {
     public partial class File
     {
@@ -20,5 +20,27 @@ namespace Yamama
         public virtual ICollection<Alert> Alert { get; set; }
         public virtual ICollection<RequestInformation> RequestInformation { get; set; }
         public virtual ICollection<Task> Task { get; set; }
+    }
+}
+using System;
+using System.Collections.Generic;
+
+namespace Yamama
+{
+    public partial class File
+    {
+        public File()
+        {
+            Alert = new HashSet<Alert>();
+            RequestInformation = new HashSet<RequestInformation>();
+        }
+
+        public int Idfile { get; set; }
+        public string ParentType { get; set; }
+        public int? ParentId { get; set; }
+        public string Path { get; set; }
+
+        public virtual ICollection<Alert> Alert { get; set; }
+        public virtual ICollection<RequestInformation> RequestInformation { get; set; }
     }
 }
